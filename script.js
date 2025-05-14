6fc07accb4cb7deeade48944a567cd551ee5fb04
Richard31225 <ricorsan_724@hotmail.com>
2025-05-14 03:38:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("magic-btn").addEventListener("click", function() {
+    const surprise = document.getElementById("surprise");
+    surprise.classList.remove("hidden");
+    surprise.classList.add("fade-in");
+  
+    // opcional: lanzar confeti o efecto
+  });
+  
