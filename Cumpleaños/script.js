63a5af59237231eb729c277ed0239b54185c1eb1
Richard <ricorsan_724@hotmail.com>
2025-05-14 03:44:00 -0500

    Delete Cumpleaños directory

--- Cumpleaños/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("magic-btn").addEventListener("click", function() {
-    const surprise = document.getElementById("surprise");
-    surprise.classList.remove("hidden");
-    surprise.classList.add("fade-in");
-  
-    // opcional: lanzar confeti o efecto
-  });
-  
